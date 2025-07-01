6d5dc2cf96a95d80f5919ddb5063756e4221174f
DrxCode-tech <anietiedaniel11@gmail.com>
2025-07-01 22:50:34 +0100

    Update V3ADEX.js

--- V3ADEX.js
+++ V3ADEX.js
@@ -451,7 +451,7 @@ markBt.addEventListener('click',async (e)=>{
   const department = (Department.textContent.trim() !== 'Department') ? Department.textContent.trim() : false;
   let cours = (currentCourseDisplay.textContent.trim() !== 'No class') ? currentCourseDisplay.textContent.trim() : false;
   
-  if(!name || !regNm || !department || !course) return alert('All ADEX field must be filled!');
+  if(!name || !regNm || !department || !cours) return alert('All ADEX field must be filled!');
 
   const course = cours.replace(/\s+/g, '').toUpperCase();
   
