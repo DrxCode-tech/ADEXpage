0ce5e14d842bcc0442389eaf3df26150ec8082b4
DrxCode-tech <anietiedaniel11@gmail.com>
2025-07-09 10:24:34 +0100

    Update review.js

--- review.js
+++ review.js
@@ -128,7 +128,7 @@ button.addEventListener('click',async()=>{
     text = `
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="24" height="24"><path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
       </svg>
-      <div class="processed-text">successful</div>
+      <div class="processed-text">Thanks for the review 🤝</div>
     `;
     process(true,text);
   }catch(err){
