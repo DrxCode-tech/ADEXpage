ed127d4ba73f1c88a96a62e334c0ceead3ea03ae
DrxCode-tech <anietiedaniel11@gmail.com>
2025-07-23 03:11:21 +0100

    Update ADEXsign.js

--- ADEXsign.js
+++ ADEXsign.js
@@ -173,7 +173,7 @@ async function createUserAcct(user,name,regNm,email,dept,level){
       lockStateDate:'',
     }
   };
-  const reg = regNm.replace('/','_');
+  const reg = regNm.replace('/','');
   try{
     const docm = doc(db,'UNIUYO',level,dept,reg);
     const emailDocm = doc(db,'EmailIndex',level,email,reg);
