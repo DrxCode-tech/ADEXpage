2ed8aa879e1da53bcdee8f356255b5620aa29c16
DrxCode-tech <anietiedaniel11@gmail.com>
2025-12-07 03:03:49 +0100

    yes

--- V3ADEX.js
+++ V3ADEX.js
@@ -1017,6 +1017,7 @@ markBt.addEventListener('click', async (e) => {
   await runGeo();
   if (!markGeoState) {
     statusDisplay(false, 'You are not in the class location, please move to the class location to mark attendance.');
+    spinnerContainer.style.display = 'none';
     return;
   }
   const dateSlash = getFormattedDate();
